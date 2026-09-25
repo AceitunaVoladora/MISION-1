bfae84aa84c23e1f9f6c6d6678e75da948cf733f
AceitunaVoladora <martamartinm.202@gmail.com>
2026-09-25 20:24:14 +0200

    preuba read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 README
+
+
+problema:
+
+```
+random = Math.floor(Math.random() * max);
+sucesion[9 - max] = random;
+
+document.getElementById(random).classList.add('activo');
+```
--- mision1.js
+++ mision1.js
@@ -1,27 +1,40 @@
-let caja1 = document.getElementById("1");
-let caja2 = document.getElementById("2");
-let caja3 = document.getElementById("3");
-let caja4 = document.getElementById("4");
-let caja5 = document.getElementById("5");
-let caja6 = document.getElementById("6");
-let caja7 = document.getElementById("7");
-let caja8 = document.getElementById("8");
-let caja9 = document.getElementById("9");
+const caja1 = document.getElementById("1");
+const caja2 = document.getElementById("2");
+const caja3 = document.getElementById("3");
+const caja4 = document.getElementById("4");
+const caja5 = document.getElementById("5");
+const caja6 = document.getElementById("6");
+const caja7 = document.getElementById("7");
+const caja8 = document.getElementById("8");
+const caja9 = document.getElementById("9");
 
-let botonEmpezar = document.querySelector("#empezar");
-let modoOscuro = document.querySelector("#oscuro");
+const botonEmpezar = document.querySelector("#empezar");
+const modoOscuro = document.querySelector("#oscuro");
 
 let restantes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let sucesion = [];
 let jugando = false;
+let random = 0;
+let max = 9;
 
 const cajas = document.querySelectorAll('.caja');
 
 function jugar(){
     cajas.forEach(caja => { 
-        caja.addEventListener('click', () => {
-            if (!jugando) return;
+        if (!jugando) return;
+        //que se encienda la sucesion anterior si hay
+        
+        
+        random = Math.floor(Math.random() * max);
+        sucesion[9 - max] = random;
 
+        document.getElementById(random).classList.add('activo');
+            
+        setTimeout(() => {
+            caja.classList.remove('activo');
+        }, 300);
+
+        caja.addEventListener('click', () => {
             caja.classList.add('activo');
             
             setTimeout(() => {
